49a4a10d087c9f72f232acd61a19f8346b7d28bc
MrBellon <104333083+MrBellon@users.noreply.github.com>
2022-09-22 09:16:32 +0900

    feat : 등록 페이지 및 로그인 마크업 완성

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import "./App.css";
+import PlaceRegistration from "./pages/PlaceRegistration";
+import Login from "./pages/Login";
+
+import styled from "styled-components";
 
 function App() {
-
   return (
     <div className="App">
-      APP
+      {/* <PlaceRegistration /> */}
+      <Login />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
